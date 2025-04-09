1aadb138cfb57534bfab33e7099640f5a9294bd1
flexirexi <felix_lehmann@ymail.com>
2025-04-09 20:12:57 +0200

    test: write test for theme toggle part 2

--- static/tests/jest/theme.test.js
+++ static/tests/jest/theme.test.js
@@ -1,12 +1,22 @@
-const { beforeEach } = require('node:test');
-const { toggleTheme } = require('static/js/theme');
+/**
+ * @jest-environment jsdom
+ */
+const { toggleClick } = require('../../js/theme.js');
 
 beforeEach(() => {
+    document.documentElement.setAttribute('theme', 'dark');
     document.body.innerHTML =       `
         <button id="theme-toggle">
-            <span id="theme-icon">🌞</span>
+            <span id="theme-icon"><i class="fas fa-sun" style="color: #b7b7b7;"></i></span>
         </button>
-        `
+        `;
 });
 
-describe("")
+describe("toggle test", () => {
+    test("expects the toggle button symbol to change to a moon symbol", () => {
+        toggleClick();
+        const theme_icon = document.getElementById("theme-icon");
+        const icon = theme_icon.querySelector("i");
+        expect(icon.classList.contains("fa-moon")).tobe(true);
+    });
+});
