9db87b8e2f17ec010e9fd7ecae3e8ae100d95364
flexirexi <felix_lehmann@ymail.com>
2025-04-11 00:06:01 +0200

    feat:  add other sections

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,21 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggle = document.getElementById("theme-toggle");
-    const themeIcon = themeToggle.querySelector("i");
-    const html = document.documentElement;
-
-    
     // const savedTheme = localStorage.getItem("theme") || "dark";
     // html.setAttribute("theme", savedTheme);
     // themeIcon.classList.add(savedTheme === "light" ? "fa-moon" : "fa-sun");
+    
 
-    // Event-Listener für den Theme-Switch
-    themeToggle.addEventListener("click", () => {
-        const newTheme = html.getAttribute("theme") === "dark" ? "light" : "dark";
-        html.setAttribute("theme", newTheme);
-        //localStorage.setItem("theme", newTheme);
 
-        // change icon
-        themeIcon.classList.remove("fa-moon", "fa-sun");
-        themeIcon.classList.add(newTheme === "dark" ? "fa-sun": "fa-moon");
-    });
+    themeToggle.addEventListener("click", toggler);
+
 });
+
+function toggler() {
+    const themeToggle = document.getElementById("theme-toggle");
+    const themeIcon = themeToggle.querySelector("i");
+    const html = document.documentElement;
+
+    const newTheme = html.getAttribute("theme") === "dark" ? "light" : "dark";
+    html.setAttribute("theme", newTheme);
+    //localStorage.setItem("theme", newTheme);
+
+    // change icon
+    themeIcon.classList.remove("fa-moon", "fa-sun");
+    themeIcon.classList.add(newTheme === "dark" ? "fa-sun": "fa-moon");
+}
