dbca442482e2fa3a81548af5e29566a2bed39f94
flexirexi <felix_lehmann@ymail.com>
2025-04-11 18:51:13 +0200

    feat: add links to main templates and move base.html to app/templates

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('rooms/', views.rooms, name="rooms"),
-    path('forum/', views.forum, name="formum"),
+    path('forum/', views.forum, name="forum"),
     path('about/', views.about, name="about"),
 ]
