71900bf519eaba0c4f65fc7212f919a898e2601e
flexirexi <felix_lehmann@ymail.com>
2025-04-12 10:33:41 +0200

    feat: make styles of main app generally user-friendly

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,12 +1,15 @@
 import { collapseNavBar } from "/static/main/js/base.js";
 
+const html = document.documentElement;
+const themeToggle = document.getElementById("theme-toggle");
+const themeIcon = themeToggle.querySelector("i");
+const savedTheme = localStorage.getItem("theme") || "dark";
+console.log(savedTheme);
+
+html.setAttribute("theme", savedTheme);
+themeIcon.classList.add(savedTheme === "light" ? "fa-moon" : "fa-sun");
 
 document.addEventListener("DOMContentLoaded", () => {
-    const themeToggle = document.getElementById("theme-toggle");
-    // const savedTheme = localStorage.getItem("theme") || "dark";
-    // html.setAttribute("theme", savedTheme);
-    // themeIcon.classList.add(savedTheme === "light" ? "fa-moon" : "fa-sun");
-    
     themeToggle.addEventListener("click", toggler);
 
 });
@@ -18,7 +21,7 @@ function toggler() {
 
     const newTheme = html.getAttribute("theme") === "dark" ? "light" : "dark";
     html.setAttribute("theme", newTheme);
-    //localStorage.setItem("theme", newTheme);
+    localStorage.setItem("theme", newTheme);
 
     // change icon
     themeIcon.classList.remove("fa-moon", "fa-sun");
