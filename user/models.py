4a6abbf96effa33a54d1f4091f207409071682fe
flexirexi <felix_lehmann@ymail.com>
2025-04-13 22:09:46 +0200

    fix: remigrating the models profile and notifications

--- user/models.py
+++ user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, 
+                                related_name="profile")
+    firstname = models.CharField(max_length=150, blank=True)
+    lastname = models.CharField(max_length=150, blank=True)
+    instruments = models.CharField(max_length=150, blank=True)
+    about = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Notifications(models.Model):
+    user_id = models.ForeignKey(Profile, on_delete=models.CASCADE,
+                                related_name="notifications")
+    message = models.TextField(max_length=1000)
+    received = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user_id}: {self.message}"
