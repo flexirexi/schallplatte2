bf60106b236183755b6d9020778815f649e37b79
flexirexi <felix_lehmann@ymail.com>
2025-04-15 13:53:23 +0200

    feat: working on the grid

--- rooms/views.py
+++ rooms/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def calendar(request):
-    return render(request, "rooms/calendar.html")
+    return render(request, "rooms/calendar.html", {
+        "hours": range(24),
+        "rooms": range(1, 9),  # do not mix up with 1 to 9-> this is 8
+    })
